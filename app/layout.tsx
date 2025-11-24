06710a1b5e111a206c3324b5a1928eca91ac2fce
Dashiell Russell <dashiell.russell@gmail.com>
2025-11-24 21:35:58 +1100

    fix: linting error

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,12 +97,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider defaultTheme="light">
           <BackgroundDots />
           {children}
           <Toaster />
